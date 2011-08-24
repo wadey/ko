164e045fae6358ba749742c1a202912fe100f5d2
Wade Simmons <wade@wades.im>
2011-08-23 19:44:40 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,72 @@
+# ko - Knockout
+
+Knockout makes it fun and easy to record your sleep / caffeine consumption
+during a programing competition. Knockout installs a git commit hook that
+records your current status in each commit message. Example:
+
+    commit 4763e15c07e679790319f6130027dd857bd0faa3
+    Author: Wade Simmons
+    Date:   Tue Aug 23 10:16:13 2011 -0700
+
+        Fix them bugs
+        
+        Hours of sleep: 7
+        Cups of coffee: 3
+        Cans of soda: 2
+        Cans of redbull: 4
+
+# Example usage
+
+    $ npm install -g ko
+    ko@0.1.0 /usr/local/lib/node_modules/ko 
+    └── commander@0.0.5
+
+    $ ko --help 
+     
+      Usage: ko [options]
+
+      Options:
+
+        -s, --sleep <hours>  Add sleep
+        -c, --coffee         Add coffee
+        -p, --soda           Add soda / pop
+        -r, --redbull        Add redbull
+
+        -l, --list           Show current stats
+        -i, --install        Install pre-commit hook
+        -R, --reset          Reset stats
+        -h, --help           output usage information
+
+    $ ko -i
+     -> .git/hooks/prepare-commit-msg
+     -> .git/hooks/commit-msg
+
+    $ ko -c 
+     
+    Hours of sleep: 0
+    Cups of coffee: 1 <<
+    Cans of soda: 0
+    Cans of redbull: 0
+
+    $ ko -s 4 
+     
+    Hours of sleep: 4 <<
+    Cups of coffee: 1
+    Cans of soda: 0
+    Cans of redbull: 0
+
+    $ git commit -a -m 'quick commit message' 
+    [master 4763e15] quick commit message
+     1 files changed, 1 insertions(+), 1 deletions(-)
+
+    $ git show HEAD 
+    commit 4763e15c07e679790319f6130027dd857bd0faa3
+    Author: Wade Simmons
+    Date:   Tue Aug 23 10:16:13 2011 -0700
+
+        quick commit message
+        
+        Hours of sleep: 4
+        Cups of coffee: 1
+        Cans of soda: 0
+        Cans of redbull: 0
---
+++ cli.js
@@ -0,0 +1,131 @@
+#!/usr/bin/env node
+
+var statsFile = process.env['HOME'] + '/.ko.json'
+  , fs = require('fs')
+  , program = require('commander')
+
+program
+
+  .option('-s, --sleep <hours>', 'Add sleep', parseInt)
+  .option('-c, --coffee', 'Add coffee')
+  .option('-p, --soda', 'Add soda / pop')
+  .option('-r, --redbull', 'Add redbull\n')
+
+  .option('-l, --list', 'Show current stats')
+  .option('-i, --install', 'Install pre-commit hook')
+  .option('-R, --reset', 'Reset stats')
+  .parse(process.argv)
+
+var options = {
+  sleep: 'Hours of sleep',
+  coffee: 'Cups of coffee',
+  soda: 'Cans of soda',
+  redbull: 'Cans of redbull',
+}
+
+var takesArgs = {
+  sleep: true
+}
+
+//
+//
+//
+
+var optionKeys = Object.keys(options)
+
+var initial = load()
+  , stats = load()
+
+// Figure out what the user asked to do
+if (program.list) {
+  list()
+} else if (program.install) {
+  install()
+} else if (program.reset) {
+  reset()
+} else if (!optionKeys.some(function(e) { return program[e] })) {
+  list()
+  console.log('\nChoose what to add:')
+  program.choose(optionKeys, function(i, e) {
+    if (takesArgs[e]) {
+      program.promptForNumber(options[e] + ': ', function(num) {
+        program[e] = num
+        process.stdin.destroy();
+        add()
+      })
+    } else {
+      program[e] = true
+      process.stdin.destroy();
+      add()
+    }
+  })
+} else {
+  add()
+}
+
+function add() {
+  var found = optionKeys.some(function(option) {
+    if (program[option]) {
+      stats[option] += program[option]
+      return true
+    }
+  })
+  
+  if (!found) throw new Error('Nothing to add')
+
+  save(stats)
+  list()
+}
+  
+function load() {
+  var stats = {}
+
+  optionKeys.forEach(function(option) {
+    stats[option] = 0
+  })
+
+  try {
+    if (fs.statSync(statsFile).isFile()) {
+      stats = JSON.parse(fs.readFileSync(statsFile, 'utf8'))
+    }
+  } catch (err) {
+    if (err.errno !== 2) throw err
+  }
+
+  return stats
+}
+
+function save(stats) {
+  fs.writeFileSync(statsFile, JSON.stringify(stats))
+}
+
+function list() {
+  console.log('\n' + optionKeys.map(function(key) {
+    var value = stats[key] + (stats[key] !== initial[key] ? ' <<' : '')
+    return options[key] + ': ' + value
+  }).join('\n'))
+}
+
+function install() {
+  function installScript(file, script) {
+    fs.writeFileSync(file, script)
+    fs.chmodSync(file, fs.statSync(file).mode | 0111)
+    console.log(' -> ' + file)
+  }
+
+  installScript('.git/hooks/prepare-commit-msg',
+      '#!/bin/sh\ngrep -qs "^'+options[optionKeys[0]]+'" "$1" || ko -l >> "$1"')
+  installScript('.git/hooks/commit-msg',
+      '#!/bin/sh\n(head -n 1 "$1" | grep -qs "^.\\+$") || echo > "$1"')
+}
+
+function reset() {
+  try {
+    fs.unlinkSync(statsFile)
+  } catch (err) {
+    if (err.errno !== 2) throw err
+  }
+  initial = load()
+  stats = load()
+  list()
+}
